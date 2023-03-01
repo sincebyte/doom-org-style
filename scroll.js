4481847d95bb325b8c32e886843924ba5810844e
vanniuner <at18708169062@gmail.com>
2023-03-01 23:30:24 +0800

    add: copy code button

--- scroll.js
+++ scroll.js
@@ -34,49 +34,18 @@ $(document).ready(function() {
 jQuery( document).ready(function($){
     var copyid = 0;
     $('.org-src-container').before().click(function (ev) {
-        console.log(111);
-        ev.preventDefault();
-        var $copyButton = $(this);
-        $pre = $(document).find('pre[data-copyid=' + $copyButton.data('copytarget' ) + ']');
-        if ( $pre.length ) {
-            var textArea = document.createElement("textarea");
-
-            // Place in top-left corner of screen regardless of scroll position.
-            textArea.style.position = 'fixed';
-            textArea.style.top = 0;
-            textArea.style.left = 0;
-
-            // Ensure it has a small width and height. Setting to 1px / 1em
-            // doesn't work as this gives a negative w/h on some browsers.
-            textArea.style.width = '2em';
-            textArea.style.height = '2em';
-
-            // We don't need padding, reducing the size if it does flash render.
-            textArea.style.padding = 0;
-
-            // Clean up any borders.
-            textArea.style.border = 'none';
-            textArea.style.outline = 'none';
-            textArea.style.boxShadow = 'none';
-
-            // Avoid flash of white box if rendered for any reason.
-            textArea.style.background = 'transparent';
-
-            //Set value to text to be copied
-            textArea.value = $pre.html();
-
-            document.body.appendChild(textArea);
-            textArea.select();
-
-            try {
-                document.execCommand('copy');
-                $copyButton.text( 'Copied').prop('disabled', true);;
-            } catch (err) {
-                $copyButton.text( 'FAILED: Could not copy').prop('disabled', true);;
-            }
-            setTimeout(function(){
-                $copyButton.text( 'Copy').prop('disabled', false);;
-            }, 3000);
-        }
+      const element = $(this);
+      const storage = document.createElement('textarea');
+      const contentCode = element.clone()
+        .find('.linenr')
+        .remove()
+        .end()
+        .text();
+      storage.value = contentCode;
+      element.append(storage);
+      storage.select();
+      storage.setSelectionRange(0, 99999);
+      document.execCommand('copy');
+      $(storage).remove();
     });
 });

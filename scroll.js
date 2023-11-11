995c51eff6126ee9742c314d2ed47a3f1ec84cbd
liu.zijie <at18708169062@gmail.com>
2023-11-11 14:30:26 +0800

    fix: css

--- scroll.js
+++ scroll.js
@@ -20,12 +20,22 @@ function getQueryVariable(variable) {
 /* 创建主题切换按钮*/
 function createCheckbox() {
   var isChecked = localStorage.getItem("doomToggleStyle");
-  let dom = `<div class="toggletheme">
-              <input class="container_toggle" type="checkbox" id="switch"  ${(isChecked == 'dark' ? 'checked' : '')} name="mode" />
-             <label for="switch">切换</label>
-            </div>`;
+  let dom = ` <div class="toggletheme">
+              <span class="white_icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="svg-icon light"><defs><style>.sun_svg__a{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px}</style></defs><circle cx="12" cy="12" r="4.5" class="sun_svg__a"></circle><path d="M12 .75V4.5M12 19.5v3.75M23.25 12H19.5M4.5 12H.75M20.25 3.75l-3 3M6.75 17.25l-3 3M20.25 20.25l-3-3M6.75 6.75l-3-3" class="sun_svg__a"></path></svg>
+              </span>
+                <input class="container_toggle" type="checkbox" id="switch"  ${(isChecked == 'dark' ? 'checked' : '')} name="mode" />
+                <label for="switch">切换</label>
+<span class="night_icon">
+              <svg xmlns="http://www.w3.org/2000/svg" id="moon_svg__Regular" viewBox="0 0 24 24" class="svg-icon dark"><defs><style>.moon_svg__cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px}</style></defs><path d="M17.235 15.75A9.752 9.752 0 0 1 7.91 3.151a9.746 9.746 0 1 0 13.4 11.7 9.689 9.689 0 0 1-4.075.899ZM11.985 3.75h4.5M14.235 1.5V6M18.735 8.25h3M20.235 6.75v3" class="moon_svg__cls-1"></path></svg>
+              </span>
+              </div>
+              `;
+
+  let buttomcheck = `<div class="btncheck"></div>`;
 
   $('body').append(dom);
+  $('body').append(buttomcheck);
 }
 
 /* 绑定界面操作事件*/
@@ -99,16 +109,27 @@ function bindEvent() {
     setTheme(this.checked ? "dark" : 'light');
   });
 
-  $('#table-of-contents').before().click(function (ev) {
-    if( $(ev.target).attr("class") == undefined
-          || $(ev.target).attr("class") == "" ){
-      $(ev.target).addClass('hidden-content');
-      $('#content').css('padding-left','0px');
-    } else {
-      $(ev.target).removeClass('hidden-content');
-      $('#content').removeClass('content-pdl');
-      $('#content').css('padding-left','240px');
-    }
+
+  $('.btncheck').click(function (ev) {
+      if( $('#table-of-contents').css("animation").indexOf("moveout") == -1){
+        $('.btncheck').css('transform','translateX(-238px)');
+        $('.btncheck').css('animation','moveout 0.2s');
+
+        $('#table-of-contents').css('transform','translateX(-240px)');
+        $('#table-of-contents').css('animation','moveout 0.2s');
+
+        $('#content').css('padding-left','0px');
+        $('#content').css('transition','padding-left 0.1s ease');
+      } else {
+        $('.btncheck').css('animation','movein 0.2s');
+        $('.btncheck').css('transform','translateX(0px)');
+
+        $('#table-of-contents').css('animation','movein 0.2s');
+        $('#table-of-contents').css('transform','translateX(0px)');
+
+        $('#content').css('padding-left','240px');
+        $('#content').css('transition','padding-left 0.1s ease');
+      }
   })
 
 
